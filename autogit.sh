d08c382b8a4d7a9de29e84723ba280a48f6dd04d
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-01-24 23:37:45 -0300

    Next Topic: “Lo-Fi” Debugging With Print | Restart tomorrow(25/01/2022)

--- autogit.sh
+++ autogit.sh
@@ -1,4 +1,4 @@
 git add -A
-git commit -m "code/index.py -> add | main.py -> alts add"
+git commit -m "Next Topic: “Lo-Fi” Debugging With Print | Restart tomorrow(25/01/2022)"
 git push -u origin master
 git status
